65f917d006940fe8bb04a1deef5abe22c9c8c72a
blackenjoy27 <lixunxin@gmail.com>
2022-02-09 10:19:46 -0500

    created mock data for movie list

---
+++ src/api/data.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        id: 1,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2021-12/Untitled%20design%20%2821%29_2.png?itok=w_EnPK-m",
+        title: "All Elite Wrestling: Rampage",
+        updateInfo: "New episode Friday at 10 p.m. ET on TNT"
+    },
+    {
+        id: 2,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-01/Euphoria%20dotcom.png?itok=RaX1cHh9",
+        title: "Euphoria",
+        updateInfo: "New episode Sunday on HBO Max"
+    },
+    {
+        id: 3,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-01/The%20Righteous%20Gemstones%20dotcom.png?itok=3te-1K35",
+        title: "The Righteous Gemstones",
+        updateInfo: "New episode Sunday at 10 p.m. ET on HBO"
+    },
+    {
+        id: 4,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-01/Untitled%20design%20%2821%29_0.png?itok=urqCy5ew",
+        title: "Somebody Somewhere",
+        updateInfo: "New episode Sunday at 10:30 p.m. ET on HBO"
+    },
+    {
+        id: 5,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-01/AEW%20Dynamite.png?itok=sjglbj6L",
+        title: "AEW: Dynamite",
+        updateInfo: "New episode Wednesday at 7 p.m. ET on TBS"
+    },
+    {
+        id: 6,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-01/Go%20Big%20Show%20dotcom.png?itok=KYceIV0W",
+        title: "Go-Big Show",
+        updateInfo: "New episode Thursday at 9:30 p.m. ET on TBS"
+    },
+    {
+        id: 7,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-02/Untitled%20design%20%2821%29.png?itok=TrGjaBnI",
+        title: "About Last Night",
+        updateInfo: "Premieres Thursday on HBO Max"
+    },
+    {
+        id: 8,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-02/The%20Girl%20Before%20dotcom.png?itok=htym4Uh9",
+        title: "The Girl Before",
+        updateInfo: "Season 1 streaming Thursday on HBO Max"
+    },
+    {
+        id: 9,
+        imgUrl: "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2022-02/Untitled%20design%20%2821%29%20copy.png?itok=oHpGuIjU",
+        title: "KIMI",
+        updateInfo: "Premieres Thursday on HBO Max"
+    }
+]
