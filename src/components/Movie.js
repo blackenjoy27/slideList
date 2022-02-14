ae56cf13229d800530c216c434ead4376aa9ce14
blackenjoy27 <lixunxin@gmail.com>
2022-02-13 23:07:45 -0500

    finish

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,20 @@
-const Movie = (props) => {
-    console.log(props);
+const Movie = ({ info }) => {
+    const { imgUrl, title, updateInfo } = info;
     return (
-        <div>
-            Movie
+        <div className="movie">
+            <a href="#"><img className="movie_cover" src={imgUrl} alt={title} /></a>
+            <div>
+                <h2>
+                    <a href="#">
+                        {title}
+                    </a>
+                </h2>
+                <p>
+                    <span>
+                        {updateInfo}
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,14 +3,77 @@ import mockData from "../api/data";
 import Movie from "./Movie";
 const MovieList = (props) => {
     const [movies, setMovies] = useState([]);
+    const [displayMovies, setDisplayMovies] = useState([]);
+    const [indexs, setIndexs] = useState({ head: 0, tail: 3 });
+    const [moves, setMoves] = useState([]);
+
+
     useEffect(() => {
         setMovies(mockData);
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (movies.length !== 0) {
+            setDisplayMovies(movies.slice(indexs.head, indexs.tail + 1))
+        }
+    }, [movies])
+
+    useEffect(() => {
+        if (moves.length !== 0) {
+            const current = moves[0];
+            if (current === "previous") {
+                const [f, s, t] = displayMovies;
+                setDisplayMovies([movies[indexs.head], f, s, t])
+
+            } else if (current === "next") {
+                const [f, ...rest] = displayMovies;
+                setDisplayMovies([...rest, movies[indexs.tail]])
+            }
+            setMoves([]);
+        }
+    }, [moves])
+
+    useEffect(() => {
+        console.log(indexs);
+    }, [indexs]);
+
+    const handleNextClick = () => {
+        if (indexs.tail === movies.length - 1) {
+            setIndexs({ head: indexs.head + 1, tail: 0 });
+        } else {
+            if (indexs.head === movies.length - 1) {
+                setIndexs({ head: 0, tail: indexs.tail + 1 })
+            } else {
+                setIndexs({ head: indexs.head + 1, tail: indexs.tail + 1 })
+            }
+        }
+        setMoves([...moves, "next"])
+    }
+
+    const handlePreviousClick = () => {
+        if (indexs.head === 0) {
+            setIndexs({ head: movies.length - 1, tail: indexs.tail - 1 });
+        } else {
+            if (indexs.tail === 0) {
+                setIndexs({ head: indexs.head - 1, tail: movies.length - 1 })
+            } else {
+                setIndexs({ head: indexs.head - 1, tail: indexs.tail - 1 })
+            }
+
+        }
+        setMoves([...moves, "previous"])
+    }
+
     return (
-        <section>
-            {movies.map(({ id, ...rest }) => {
-                return <Movie key={id} info={rest} />
-            })}
+        <section className="what_on_container">
+            <h2 className="what_on_title">WHAT'S ON</h2>
+            <div className="movie_list">
+                <button className="previous" onClick={handlePreviousClick}>❰</button>
+                {displayMovies.map(({ id, ...rest }) => {
+                    return <Movie key={id} info={rest} />
+                })}
+                <button className="next" onClick={handleNextClick}>❱</button>
+            </div>
         </section>
     )
 }
